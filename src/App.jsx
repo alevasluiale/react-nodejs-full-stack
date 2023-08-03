18838ab49d6cf6be26646ce6a9c047a318a45a98
Marius Huhurez <mariushuhurez@Mariuss-MacBook-Air.local>
2023-08-03 11:05:12 +0300

    Display results

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+// client/App.js
+import React, { useState } from "react";
+import axios from "axios";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import CircularProgress from "@mui/material/CircularProgress";
+import Paper from "@mui/material/Paper";
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async (path) => {
+    setLoading(true);
+    axios
+      .get(path)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      })
+      .finally((err) => setLoading(false));
+  };
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
+  console.log(data);
+  return (
+    <>
+      <Stack
+        spacing={2}
+        direction="row"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button variant="contained" onClick={() => fetchData("button_1")}>
+          Button 1
+        </Button>
+        <Button variant="contained" onClick={() => fetchData("button_2")}>
+          Button 2
+        </Button>
+        <Button variant="contained" onClick={() => fetchData("button_3")}>
+          Button 3
+        </Button>
+      </Stack>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {data.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.Title}</TableCell>
+                <TableCell align="right">{row.Year}</TableCell>
+                <TableCell align="right">{row.imdbID}</TableCell>
+                <TableCell align="right">{row.Poster}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+
+export default App;
