0b77be244b06506458df457e61e0bf21f0a4162a
Marius Huhurez <mariushuhurez@Mariuss-MacBook-Air.local>
2023-08-04 10:14:08 +0300

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
+import TableComponent from "./components/TableComponent";
 import CircularProgress from "@mui/material/CircularProgress";
-import Paper from "@mui/material/Paper";
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -56,46 +50,7 @@ const App = () => {
           Button 3
         </Button>
       </Stack>
-
-      {Boolean(data) && (
-        <TableContainer component={Paper} className="tableContainer">
-          <Table
-            sx={{ minWidth: 500, width: "500px", margin: "auto" }}
-            size="small"
-            aria-label="simple table"
-            className="tableContainer"
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell align="right">Title</TableCell>
-                <TableCell align="right">Year</TableCell>
-                <TableCell align="right">Imdb ID</TableCell>
-                <TableCell align="right">Poster</TableCell>
-              </TableRow>
-            </TableHead>
-
-            <TableBody>
-              {data.map((row) => (
-                <TableRow
-                  key={row.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell align="right">{row.Title}</TableCell>
-                  <TableCell align="right">{row.Year}</TableCell>
-                  <TableCell align="right">{row.imdbID}</TableCell>
-                  <TableCell align="right">
-                    {row.Poster !== "N/A" ? (
-                      <img src={row.Poster} alt="" />
-                    ) : (
-                      ""
-                    )}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      )}
+      <TableComponent data={data} />
     </>
   );
 };
