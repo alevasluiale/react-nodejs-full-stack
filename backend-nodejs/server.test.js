139b0b8938c7adea08a53202d906e3e0de1305ea
Marius Huhurez <mariushuhurez@Mariuss-MacBook-Air.local>
2023-08-04 10:58:34 +0300

    Completion

--- backend-nodejs/server.test.js
+++ backend-nodejs/server.test.js
@@ -1,43 +1,29 @@
 const request = require("supertest");
-const app = require("./server"); // Import the Express app
-// dbConnection.test.js
-const mysql = require("mysql");
+const app = require("./server");
+const axios = require("axios");
 
-// Mock the mysql.createConnection function
-jest.mock("mysql", () => ({
-  createConnection: jest.fn(() => ({
-    connect: jest.fn(),
-    query: jest.fn(),
-  })),
-}));
-
-describe("GET /button_1", () => {
-  it("should return status 200 and an array of unique movies", async () => {
+describe("GET Requests", () => {
+  it("should respond with status 200 and movie data for /button_1", async () => {
     const response = await request(app).get("/button_1");
     expect(response.status).toBe(200);
-    expect(Array.isArray(response.body)).toBe(true);
-
-    // Assuming the response contains movie objects with "Title", "Type", "Year", "imdbID", and "Poster" fields
-    const movieFields = ["Title", "Type", "Year", "imdbID", "Poster"];
-    response.body.forEach((movie) => {
-      expect(movie).toEqual(expect.objectContaining(movieFields));
-    });
   });
 
-  // Test other scenarios if needed
-});
+  it("should respond with status 200 and movie data for /button_2", async () => {
+    const response = await request(app).get("/button_2");
+    expect(response.status).toBe(200);
+  });
 
-describe("Database Connection", () => {
-  it("should create a database connection", () => {
-    // Call the Express app to initiate the database connection
-    // The mocked createConnection function will be used
-    request(app).get("/button_1");
+  it("should respond with status 200 and movie data for /button_3", async () => {
+    const response = await request(app).get("/button_3");
+    expect(response.status).toBe(200);
+  });
 
-    // Ensure mysql.createConnection was called
-    expect(require("mysql").createConnection).toHaveBeenCalled();
+  it("should handle errors when fetching movies data", async () => {
+    jest.spyOn(axios, "get").mockRejectedValue(new Error("API Error"));
 
-    // Ensure that the connect function was called on the connection object
-    const mockConnection = require("mysql").createConnection();
-    expect(mockConnection.connect).toHaveBeenCalled();
+    const response = await request(app).get("/button_1");
+
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual({ error: "Internal server error" });
   });
 });
